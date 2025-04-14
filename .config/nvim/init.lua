e9e6b463d1e2c458946029a03ffd0d0b4c07326c
Lars Andersson <and502@gmail.com>
2025-04-14 13:55:38 +1000

    nvim work

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,34 @@
 require("config.lazy")
 
-vim.keymap.set("i", "jk", "<Esc>", {remap = true})
-vim.keymap.set({"n", "v"}, "<leader>c", "gc", {remap = true})
-vim.keymap.set({"n"}, "<leader>cc", "gcc", {remap = true}) 
+vim.keymap.set("i", "jk", "<Esc>", { remap = true })
+vim.keymap.set({ "n", "v" }, "<leader>c", "gc", { remap = true })
+vim.keymap.set({ "n" }, "<leader>cc", "gcc", { remap = true })
 
+-- Save file shortcut
+vim.keymap.set({ "n" }, "<leader>w", ":w!<cr>", { remap = true })
 
-vim.keymap.set({"n"}, "<leader>w", ":w!<cr>", {remap = true})
+-- Edit file in the same directory
+vim.keymap.set('n', '<leader>e', ':e <C-R>=expand("%:p:h") . "/" <CR>', {})
+vim.keymap.set('n', '<leader>X', ':x<cr>', {})
+vim.keymap.set('n', '<leader>j', ':e #<cr>', {})
 
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>m', builtin.buffers, {})
+vim.o.wildmode = 'longest,list,full'
+vim.o.wildmenu = true
+
+vim.o.number = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.opt.matchpairs:append("<:>")
+
+vim.o.tabstop = 4
+vim.o.sw = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
+-- vim.cmd.colorscheme('iceberg')
+
+-- Example config in lua
+
+-- Load the colorscheme
+-- require('nord').set()
---
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -0,0 +1,65 @@
+return {
+    {
+        "shaunsingh/nord.nvim",
+        priority = 10,
+        config = function()
+            -- Optionally configure and load the colorscheme
+            -- directly inside the plugin declaration.
+            vim.g.nord_contrast = true
+            vim.g.nord_borders = false
+            vim.g.nord_disable_background = false
+            vim.g.nord_italic = false
+            vim.g.nord_uniform_diff_background = true
+            vim.g.nord_bold = false
+            vim.cmd.colorscheme('nord')
+        end
+    },
+    {
+        "oahlen/iceberg.nvim",
+        name = "iceberg",
+        config = function()
+            -- vim.cmd.colorscheme('iceberg')
+        end
+    },
+    {
+        "sainnhe/everforest",
+        lazy = false,
+        priority = 100,
+        config = function()
+            -- Optionally configure and load the colorscheme
+            -- directly inside the plugin declaration.
+            vim.g.everforest_enable_italic = false
+            vim.g.everforest_background = 'hard'
+            vim.cmd.colorscheme('everforest')
+        end
+    },
+    -- { "EdenEast/nightfox.nvim" }, -- lazy
+    -- { "catppuccin/nvim", name = "catppuccin" },
+    --     {
+    --   "thesimonho/kanagawa-paper.nvim",
+    --   lazy = false,
+    --   priority = 1000,
+    --   opts = {},
+    -- },
+    -- {
+    --     "mcauley-penney/ice-cave.nvim",
+    --     config = function()
+    --         vim.cmd.colorscheme("ice-cave")
+    --     end,
+    --     priority = 1000
+    -- }
+    -- {
+    --   'projekt0n/github-nvim-theme',
+    --   name = 'github-theme',
+    --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    --   priority = 1000, -- make sure to load this before all the other start plugins
+    --   config = function()
+    --     require('github-theme').setup({
+    --       -- ...
+    --     })
+    --
+    --     vim.cmd('colorscheme github_dark')
+    --   end,
+    --
+    --   }
+}
