b581b1bdebf9befe0cc4fbc3e127ac1656f173da
Lars Andersson <and502@gmail.com>
2025-04-25 08:51:27 +1000

    nvim telescope keymappings

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,10 +1,22 @@
--- Add Telescope
 return {
     {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.8',
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
+            require('telescope').setup {
+                -- You can put your default mappings / updates / etc. in here
+                --  All the info you're looking for is in `:help telescope.setup()`
+                --
+                defaults = {
+                    mappings = {
+                        i = {
+                            ["<C-j>"] = require('telescope.actions').move_selection_next,
+                            ["<C-k>"] = require('telescope.actions').move_selection_previous,
+                        },
+                    },
+                },
+            }
             -- Telescope key mapping
             local builtin = require("telescope.builtin")
             vim.keymap.set('n', '<C-p>', builtin.find_files, {})
