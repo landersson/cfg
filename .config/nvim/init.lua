2d40d8b6af8ad5b4bbbff966bc465e5b6f0dcfcf
Lars Andersson <and502@gmail.com>
2025-08-25 09:16:23 +1000

    Add neovide configuration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -94,3 +94,19 @@ if vim.fn.has("mac") == 1 then
   -- Your macOS-specific Neovim commands here
   vim.o.shell = "/opt/homebrew/bin/bash"
 end
+
+-- Neovide configuration
+if vim.g.neovide then
+  -- Disable all animations
+  vim.g.neovide_position_animation_length = 0
+  vim.g.neovide_cursor_animation_length = 0.00
+  vim.g.neovide_cursor_trail_size = 0
+  vim.g.neovide_cursor_animate_in_insert_mode = false
+  vim.g.neovide_cursor_animate_command_line = false
+  vim.g.neovide_scroll_animation_far_lines = 0
+  vim.g.neovide_scroll_animation_length = 0.00
+
+  if vim.fn.has("mac") == 1 then
+    vim.o.guifont = "Menlo:h18"
+  end
+end
