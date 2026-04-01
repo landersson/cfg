633649e7c769fa4bf5db8e7480b404746bd5645b
Lars Andersson <and502@csiro.au>
2026-04-01 13:31:04 +1100

    Add new nvim colorschemes

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -79,6 +79,28 @@ require("lazy").setup({
         end, { desc = "Open LazyGit" })
       end,
     },
+    {
+      'AlexvZyl/nordic.nvim',
+      lazy = false,
+      priority = 1000,
+      config = function()
+        require('nordic').load()
+      end
+    },
+    {
+      "zenbones-theme/zenbones.nvim",
+      -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+      -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+      -- In Vim, compat mode is turned on as Lush only works in Neovim.
+      dependencies = "rktjmp/lush.nvim",
+      lazy = false,
+      priority = 1000,
+      -- you can set set configuration options here
+      -- config = function()
+      --     vim.g.zenbones_darken_comments = 45
+      --     vim.cmd.colorscheme('zenbones')
+      -- end
+    },
     { import = "plugins" },
   },
   -- Configure any other settings here. See the documentation for more details.
